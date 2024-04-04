ec431b1c2aebd56945a6f381b1f07ab44d918486
Jash Gusani <143817091+Jashgusani123@users.noreply.github.com>
2024-04-05 00:15:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let Div = document.getElementById("mydiv");
+Div.style.backgroundColor = "yellow";
+Div.style.margin = "20px";
+Div.style.padding = "10px";
+Div.style.fontSize = "18px";
+Div.style.fontWeight = "Bold";
+Div.style.height = "200px";
+Div.style.width = "300px";
+
